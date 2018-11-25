fe547ba568dc8c5657de47d6637ba96604560216
RPilatti <patrickpilatti1@gmail.com>
2018-11-25 13:01:51 -0800

    button styling, logic overhaul

--- logic.js
+++ logic.js
@@ -2,10 +2,51 @@
 //======================================
 
 
-//Console logs the string "Ay yo".
-function ayYo() {
-console.log("Ay yo")
+//SEARCH PARAMETERS
+//======================================
 
-};
 
-ayYo();
+//BASE URL
+//======================================
+
+
+//FUNCTIONS
+//======================================
+$(document).ready(function () {
+    //Enter button fix to stop refreshing the page
+        $("#searchTerm").keyup(function (event) {
+            if (event.keyCode == 13) {
+                $("#submit").click();
+            }
+        });
+    });
+
+//MAIN PROCESSES
+//======================================
+// $(document).ready(function() {
+
+//     $('#search').click(function(){
+        
+//         var searchTerm = $('searchTerm').val();
+
+//         var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+
+//         $.ajax({
+//             type:"GET",
+//             url:url,
+//             async: false,
+//             dataType: "json",
+//             success:function(data){
+//                 console.log(data[1][0]);
+//                 console.log(data[2][0]);
+//                 console.log(data[3][0])
+//             },
+//             error: function(errorMessage){
+//             alert("Error");
+//             }
+//         })
+//     });
+
+// });
+
+console.log("test");
